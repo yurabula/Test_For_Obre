25067733b53898ebdedb6545b5a3b3bd405fe455
yurabula <bulayuram@gmail.com>
2024-07-26 15:06:20 +0300

    Add local storage operation

--- src/localStorage/localStorage.js
+++ src/localStorage/localStorage.js
@@ -2,4 +2,21 @@ const saveToLocalStorage = (key, data) => {
   localStorage.setItem(key, JSON.stringify(data));
 };
 
-const getFromLocalStorage = (key)
+const getFromLocalStorage = (key) => {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : [];
+};
+
+const addItemToLocalStorage = (key, newItem) => {
+  const data = getFromLocalStorage(key);
+  data.push(newItem);
+  saveToLocalStorage(key, data);
+};
+
+const removeItemFromLocalStorage = (key, itemId) => {
+  const data = getFromLocalStorage(key);
+  const updatedData = data.filter((item) => item.id !== itemId);
+  saveToLocalStorage(key, updatedData);
+};
+
+export {saveToLocalStorage, getFromLocalStorage, addItemToLocalStorage, removeItemFromLocalStorage}
